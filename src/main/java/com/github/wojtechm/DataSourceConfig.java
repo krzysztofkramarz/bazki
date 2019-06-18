8955c97ad1b2a94200d80605b4a0060eee8a3a4a
WojTechM <wojciech.m1997@gmail.com>
2019-06-18 17:50:34 +0200

    Uzupełnienie i poprawka dokumentacji

--- src/main/java/com/github/wojtechm/DataSourceConfig.java
+++ src/main/java/com/github/wojtechm/DataSourceConfig.java
@@ -3,6 +3,27 @@ package com.github.wojtechm;
 import java.util.ResourceBundle;
 
 /**
+ * Chamskie zaciągnięcie do kodu konfiguracji z application.properties.
+ *
+ * Zapytasz pewnie:
+ * "Ale Wojciech! Skoro i tak wciągasz całą zawartość do kodu, to czemu nie usuniesz pliku
+ *      .properties całkiem, a zawartości wstawisz tutaj?"
+ *
+ * Znakomite pytanie!
+ *
+ * Poniżej znajdziesz serię wymówek, dlaczego popełniłem ten okropny kawałek kodu, ale nie są one istotne dla
+ * zrozumienia całości tego repozytorium, więc możesz je ominąć.
+ *
+ * Jednak czytasz? Dobrze. A więc...
+ * Odpowiedź jest dość prosta: wypchnięcie konfiguracji do jednego miejsca.
+ * Nie chcemy szukać konfiguracji po całym kodzie - chcemy ją mieć w jednym miejscu.
+ * Pozwala nam to na łatwiejsze modyfikacje oraz poprawia spójności aplikacji.
+ * Dodatkowo, przy aplikacjach Spring Bootowych swego rodzaju odruchem jest szukanie konfiguracji
+ * w pliku application.properties właśnie.
+ *
+ * Oczywiście tej aplikacji nikt nie będzie rozszerzał (a przynajmniej wątpię by ktoś chciał to robić)
+ * więc jest to lekka przesada, ale cóż... decyzje zostały podjęte.
+ *
  * @author Wojciech Makiela
  */
 public class DataSourceConfig {
---
+++ src/main/java/com/github/wojtechm/jdbc/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * W tym pakiecie znajdziesz gotowe rozwiązania obrazujące jOOQ oraz Spring JDBC Template.
+ * <br/><br/>
+ * Najlepszym sposobem na zapoznanie się z kodem będzie przejażdżka debuggerem zaczynając
+ * w metodzie `run`, pomijając konstruktory (bo po co nam to? cnie?).
+ * <br/><br/>
+ * Nie polecam płynąć zbyt głęboko - tj. w kod Springa / jOOQa. To znaczy możesz, ale gorąco odradzam.
+ * <br/><br/>
+ * W implementacji używającej jOOQa znajdziesz jedną metodę której ciało w większości jest
+ * zakomentowane, oraz opatrzone komentarzem TODO.
+ * Jest to kawałek którego nie udało się nam ogarnąć. Kod został aby dać znać gdzie skończyliśmy pracę.
+ * Legendy głoszą, że raz na 100 akademii pojawia się bohater, i że bohater ten naprawi tą metodę!
+ * <br/><br/>
+ * Może chcesz podjąć wyzwanie? :D
+ * <br/><br/>
+ * @author Wojciech Makiela
+ */
+package com.github.wojtechm.jdbc;
